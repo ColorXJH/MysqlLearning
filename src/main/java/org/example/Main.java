b407fbf3680a06b683d80022c3a9b590f68c6bba
ColorXJH <1204332316@qq.com>
2024-02-19 19:17:46 +0800

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+/**
+ * @ClassName: ${NAME}
+ * @Package: org.example
+ * @Description:
+ * @Datetime: 2024/2/18 20:17
+ * @author: ColorXJH
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/test/java/Test.java
@@ -0,0 +1,9 @@
+/**
+ * @ClassName: Test
+ * @Package: PACKAGE_NAME
+ * @Description:
+ * @Datetime: 2024/2/18 20:28
+ * @author: ColorXJH
+ */
+public class Test {
+}
